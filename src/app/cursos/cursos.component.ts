66b9b8690b033269def9699f3120ed1e738f97eb
Ana Maria <ana.msilva@hotmail.com>
2022-11-09 17:07:08 -0300

    servicos e injeção de dependencia

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CursosService } from './cursos.service';
+
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
@@ -8,16 +10,16 @@ import { Component, OnInit } from '@angular/core';
 export class CursosComponent implements OnInit {
 
   nomePortal: string;
-  cursos: string[] = ['Java','Ext Js','Angular'];
 
-  constructor() {
+  cursos: string[];
+
+  constructor(private cursosService: CursosService) {
 
     this.nomePortal = 'https://loiane.training';
 
-    // for (let i=0; i<this.cursos.lenght; i++) {
-    //   let curso = this.cursos[i];
-    //   }
-   }
+    this.cursos = this.cursosService.getCursos();
+
+  }
 
 
   ngOnInit(): void {
--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { CursosComponent } from './cursos.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos.service';
 
 
 
@@ -19,6 +20,10 @@ import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
   exports: [
     CursosComponent
+  ],
+
+  providers: [
+    CursosService
   ]
 })
 export class CursosModule { }
